ed92eb859aeea96e80db81ff07112fb04013ef42
Paul Novak <pnovak@alumni.caltech.edu>
2021-03-10 14:45:14 -0800

    Use a black background

--- html/construal_level_task-legacy-browsers.js
+++ html/construal_level_task-legacy-browsers.js
@@ -10,7 +10,7 @@ const psychoJS = new PsychoJS({
 // open window:
 psychoJS.openWindow({
   fullscr: true,
-  color: new util.Color([0, 0, 0]),
+  color: new util.Color([-1, -1, -1]),
   units: 'height',
   waitBlanking: true
 });
