5435085a53727f3eb0975b79c21eaa0a85172819
Phuong Le <phuongqnhle@gmail.com>
2021-03-30 16:24:34 -0400

    update instructions

--- construal_level_task_lastrun.py
+++ construal_level_task_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.2),
-    on March 24, 2021, at 16:03
+    on March 30, 2021, at 16:22
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
